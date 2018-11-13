f9db0690f37b0a86ced8135b3def9ec73f64422c
Justin Sobieski <jmksobieski@gmail.com>
2018-11-12 21:52:31 -0500

    Update bot.py

--- ProbsJustin/bot.py
+++ ProbsJustin/bot.py
@@ -1,10 +1,28 @@
 #rewrite
-#?gen [words/paragraphs] [int]
+#?gen [words/paragraphs/list] [int]
 import discord
 from discord.ext import commands
 import random
 import aiohttp
+import json
+import os
 bot = commands.Bot(command_prefix='?')
+
+
+def start():
+    if not os.path.exists(f"./config.json"):
+        print('Config not found, creating it.')
+        with open('config.json', 'w+') as f:
+            data = {"config": {"token": ""}}
+            json.dump(data, f)
+
+    with open('config.json', 'r') as f:
+        data = json.load(f)
+        if not data.get('config').get('token'):
+            print('please enter your token in config.json')
+            os.system("pause")
+        token = data.get('config').get('token')
+        return token
 
 
 @bot.event
@@ -120,8 +138,26 @@ async def get_url(content):
         return url
 
 
+async def download_scripts():
+    if not os.path.exists(f"./scripts.txt"):
+        async with bot.aiohttp.get(url='https://cgg.website/dbchallenge/scripts.txt') as resp:
+            filename = os.path.basename('scripts.txt')
+            with open(filename, 'wb') as f_handle:
+                while True:
+                    chunk = await resp.content.read(1024)
+                    if not chunk:
+                        break
+                    f_handle.write(chunk)
+            return await resp.release()
+    else:
+        return
+
+
 async def create_aiohttp():
     bot.aiohttp = aiohttp.ClientSession()
 
+
 bot.loop.create_task(create_aiohttp())
-bot.run('')
+bot.loop.create_task(download_scripts())
+bot.run(start())
+
